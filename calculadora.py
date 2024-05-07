4c229836e7d98f78126776d83749e2463802978e
sabinygabrielle <00001119178769sp@al.educacao.sp.gov.br>
2024-05-07 18:09:57 -0300

    Create calculadora.py

---
+++ calculadora.py
@@ -0,0 +1,8 @@
+def soma(a, b):
+    return a + b
+
+if __name__ == "__main__";
+  num1 = float(input("Digite o primeiro numero: " ))
+  num2 = float(input("Digite o segundo numero: " ))
+  resultad = soma(num1, num2)
+  print(f"resultado: {resultado}")
